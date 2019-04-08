916a6a136165485f7f1ca0f3a036cc62f8d93cca
Sushant Deshmukh <riekmenrudolf@gmail.com>
2019-04-08 16:05:59 -0500

    Update cj2019p1.py

--- cj2019p1.py
+++ cj2019p1.py
@@ -1,3 +1,23 @@
+#Problem
+#Someone just won the Code Jam lottery, and we owe them N jamcoins! 
+#However, when we tried to print out an oversized check, we encountered a problem. 
+#The value of N, which is an integer, includes at least one digit that is a 4...
+#and the 4 key on the keyboard of our oversized check printer is broken.
+
+#Fortunately, we have a workaround: we will send our winner two checks for positive integer amounts A and B, 
+#such that neither A nor B contains any digit that is a 4, and A + B = N. 
+#Please help us find any pair of values A and B that satisfy these conditions.
+# Sample Input 
+#3
+#4
+#940
+#4444
+
+#Output 
+  
+#Case #1: 2 2
+#Case #2: 852 88
+#Case #3: 667 3777
 
 def blackmagic(x):
     l = (list(str(x)))
@@ -15,10 +35,3 @@ if __name__ == '__main__':
     for i in range(int(input())):
         num=blackmagic(int(input()))
         print("Case #"+str(i+1)+": "+str(num[0])+" "+str(num[1]))
-
-
-
-
-
-
-
